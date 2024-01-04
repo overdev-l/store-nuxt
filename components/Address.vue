571ad307d4b22882bf41f74ba2f990ff27100acd
姚欢 <1046540150@qq.com>
2024-01-04 17:59:42 +0800

    添加收货地址

--- components/Address.vue
+++ components/Address.vue
@@ -1,16 +1,44 @@
 <template>
-  <ClientOnly>
-    <el-drawer :model-value="value" title="收货地址" direction="rtl" :before-close="close">
-      <div class="w-full h-fit flex flex-col gap-2">
-        <AddressItem />
-        <AddressItem />
-        <AddressItem />
-        <AddressItem />
-        <AddressItem />
-      </div>
-    </el-drawer>
-
-  </ClientOnly>
+  <div>
+    <ClientOnly>
+      <el-drawer :model-value="value" title="收货地址" direction="rtl" :before-close="close">
+        <div class="w-full h-fit flex flex-col gap-2">
+          <AddressItem />
+          <AddressItem />
+          <el-button @click="addressProp" type="primary" round>添加收货地址</el-button>
+        </div>
+      </el-drawer>
+    </ClientOnly>
+    <el-dialog v-model="dialogVisible" title="添加收货地址" width="30%" :before-close="handleClose">
+      <el-form   ref="ruleFormRef"  :rules="rules" :model="form" label-width="130px">
+        <el-form-item label="收货人:" prop="name">
+          <el-input v-model="form.name" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item label="手机号:" prop="phone">
+          <el-input v-model="form.phone" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item label="所在地区:" prop="addressList">
+          <el-cascader placeholder="请选择" style="width: 100%;" v-model="form.addressList" :options="options" :props="props"
+            @change="handleChange">
+          </el-cascader>
+        </el-form-item>
+        <el-form-item label="详细地址:" prop="textarea">
+          <el-input v-model="form.textarea" :rows="2" type="textarea" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item label="设为默认收货地址:" prop="acquiesce">
+          <el-switch v-model="form.acquiesce" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="cancel">取消</el-button>
+          <el-button type="primary" @click="submitForm(ruleFormRef)">
+            确认
+          </el-button>
+        </span>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -20,8 +48,117 @@ defineProps<{
   value: boolean
 }>()
 const emits = defineEmits(["close"])
-
-const close =() => {
+const dialogVisible = ref(false)
+const form = ref<any>({
+  name: '',
+  phone: '',
+  addressList: [],
+  textarea: '',
+  acquiesce: false,
+})
+const options = ref<any[]>([])
+const props = {
+  expandTrigger: 'hover' as const,
+  value: 'id', // 对应每个选项的值
+  label: 'name', // 对应每个选项的显示文本
+  children: 'children' // 对应每个选项的子选项
+}
+const ruleFormRef = ref<any>()
+const rules = reactive<any>({
+  name: [
+    { required: true, message: '请输入', trigger: 'blur' },
+  ],
+  phone: [
+    {
+      required: true,
+      message: '请输入',
+      trigger: 'change',
+    },
+    { min: 11, max: 11, message: '请输入正确的手机号', trigger: 'blur' },
+  ],
+  addressList: [
+    { required: true, message: '请选择', trigger: 'blur' },
+  ],
+  textarea: [
+    { required: true, message: '请输入', trigger: 'blur' },
+  ],
+})
+const close = () => {
   emits("close")
 }
+const addressData = async () => {
+  const { code, data, message } = await request<any>({
+    url: '/api/address/locations',
+    method: 'post',
+    data: {}
+  })
+  options.value = data
+  const processedData = data.map((item:any) => replaceKeys(item));
+  console.log(processedData, 'processedData')
+  options.value = processedData;
+}
+const addressProp = () => {
+  dialogVisible.value = true
+  addressData()
+}
+const handleClose = () => {
+
+}
+const handleChange = () => {
+
+}
+function replaceKeys(item: any) {
+  // 替换 cities 为 children
+  if (item.cities && item.cities.length > 0) {
+    item.children = item.cities.map(replaceKeys);
+    delete item.cities;
+  }
+
+  // 替换 areas 为 children
+  if (item.areas && item.areas.length > 0) {
+    item.children = item.areas.map(replaceKeys);
+    delete item.areas;
+  }
+
+  // 替换 streets 为 children
+  if (item.streets && item.streets.length > 0) {
+    item.children = item.streets.map(replaceKeys);
+    delete item.streets;
+  }
+
+  // 删除空的 children 数组
+  if (item.children && item.children.length === 0) {
+    delete item.children;
+  }
+
+  return item;
+}
+const cancel = () => {
+  form.value.name = ''
+  form.value.phone = ''
+  form.value.addressList = []
+  form.value.textarea = ''
+  form.value.acquiesce = false
+  dialogVisible.value = false
+}
+
+const submitForm = async (formEl: any) => {
+  console.log(formEl);
+  
+  if (!formEl) return
+  await formEl.validate((valid:any, fields:any) => {
+    if (valid) {
+      dialogVisible.value = false
+      ElMessage({
+        message: '添加成功',
+        type: 'success',
+      })
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+}
+onMounted(() => {
+  addressData
+})
 </script>
---
+++ pages/manageOrders.vue
@@ -0,0 +1,293 @@
+<template>
+     <div class="w-full h-full">
+        <div class="w-full h-fit">
+            <form action="" class="flex gap-2 items-end" ref="formRef" @submit.prevent>
+                <label class="form-control w-full max-w-xs">
+                    <div class="label">
+                        <span class="label-text">订单id</span>
+                    </div>
+                    <input type="text" placeholder="订单id" class="input input-sm input-bordered input-info w-full max-w-xs"
+                        v-model="orderId" />
+                </label>
+                <label class="form-control w-full max-w-xs">
+                    <div class="label">
+                        <span class="label-text">商品名称</span>
+                    </div>
+                    <input type="text" placeholder="订单名称" class="input input-sm input-bordered input-info w-full max-w-xs"
+                        v-model="orderName" />
+                </label>
+                <button class="btn btn-outline btn-info btn-sm" @click="submit">查询</button>
+                <button type="reset" class="btn btn-outline  btn-sm" @click="reset">重置</button>
+            </form>
+        </div>
+        <el-table :data="tableData" :border="parentBorder" style="width: 100%">
+    <el-table-column type="expand">
+      <template #default="props">
+        <div m="4">
+          <p m="t-0 b-2">State: {{ props.row.state }}</p>
+          <p m="t-0 b-2">City: {{ props.row.city }}</p>
+          <p m="t-0 b-2">Address: {{ props.row.address }}</p>
+          <p m="t-0 b-2">Zip: {{ props.row.zip }}</p>
+          <!-- <h3>Family</h3> -->
+          <!-- <el-table :data="props.row.family" :border="childBorder">
+            <el-table-column label="Name" prop="name" />
+            <el-table-column label="State" prop="state" />
+            <el-table-column label="City" prop="city" />
+            <el-table-column label="Address" prop="address" />
+            <el-table-column label="Zip" prop="zip" />
+          </el-table> -->
+        </div>
+      </template>
+    </el-table-column>
+    <el-table-column label="Date" prop="date" />
+    <el-table-column label="Name" prop="name" />
+  </el-table>
+     </div>
+</template>
+<script setup lang="ts">
+import type { FormInstance, UploadRequestHandler, FormRules } from 'element-plus';
+definePageMeta({
+    layout: 'admin'
+})
+const parentBorder = ref(false)
+const childBorder = ref(false)
+const orderId = ref('')
+const orderName = ref('')
+const page = ref(1)
+const limit = ref(10)
+const tableData = ref<any>([
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'San Francisco',
+    address: '3650 21st St, San Francisco',
+    zip: 'CA 94114',
+    family: [
+      {
+        name: 'Jerry',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Spike',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Tyke',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+    ],
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'San Francisco',
+    address: '3650 21st St, San Francisco',
+    zip: 'CA 94114',
+    family: [
+      {
+        name: 'Jerry',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Spike',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Tyke',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+    ],
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'San Francisco',
+    address: '3650 21st St, San Francisco',
+    zip: 'CA 94114',
+    family: [
+      {
+        name: 'Jerry',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Spike',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Tyke',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+    ],
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'San Francisco',
+    address: '3650 21st St, San Francisco',
+    zip: 'CA 94114',
+    family: [
+      {
+        name: 'Jerry',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Spike',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Tyke',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+    ],
+  },
+  {
+    date: '2016-05-08',
+    name: 'Tom',
+    state: 'California',
+    city: 'San Francisco',
+    address: '3650 21st St, San Francisco',
+    zip: 'CA 94114',
+    family: [
+      {
+        name: 'Jerry',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Spike',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Tyke',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+    ],
+  },
+  {
+    date: '2016-05-06',
+    name: 'Tom',
+    state: 'California',
+    city: 'San Francisco',
+    address: '3650 21st St, San Francisco',
+    zip: 'CA 94114',
+    family: [
+      {
+        name: 'Jerry',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Spike',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Tyke',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+    ],
+  },
+  {
+    date: '2016-05-06',
+    name: 'Tom',
+    state: 'California',
+    city: 'San Francisco',
+    address: '3650 21st St, San Francisco',
+    zip: 'CA 94114',
+    family: [
+      {
+        name: 'Jerry',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Spike',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+      {
+        name: 'Tyke',
+        state: 'California',
+        city: 'San Francisco',
+        address: '3650 21st St, San Francisco',
+        zip: 'CA 94114',
+      },
+    ],
+  },
+    ])
+const total = ref()
+const submit = () => {
+    page.value = 1
+    limit.value = 10
+    submitQuery()
+}
+const submitQuery = () => {
+
+}
+const reset = () => {
+    page.value = 1
+    limit.value = 10
+    orderId.value = ''
+    orderName.value = ''
+    submitQuery()
+}
+</script>
