c8a95ffa72f12ecb2a4f542dddd39815e688ed31
姚欢 <1046540150@qq.com>
2024-01-11 19:03:36 +0800

    商品详情

--- pages/detail/[id].vue
+++ pages/detail/[id].vue
@@ -1,9 +1,108 @@
 <template>
     <div class="w-full h-fit">
-        detail
+        <div class="header">
+            <div class="left">
+                <img class="shopImg" :src="detailData.cover" alt="">
+            </div>
+            <el-card class="right">
+                <h2 class="title">{{ detailData.title }}</h2>
+                <p>单价: {{ detailData.price }}</p>
+                <p>重量：1.2kg</p>
+                <p>已售: 2万+</p>
+            </el-card>
+        </div>
+        <div class="footer">
+            <h1>宝贝详情</h1>
+            <el-card class="content">
+                <div style="width:100vw;display: flex;justify-content: center;justify-content: center;">
+                    <ul>
+                    <li>功效：杀菌除垢</li>
+                    <li>物理形态：粉状</li>
+                    <li>品牌：KAO/花王</li>
+                </ul>
+                <ul>
+                    <li>品名：花王洗衣机槽清洗剂6袋组合</li>
+                    <li>套餐类型：除垢消臭</li>
+                    <li>适用对象：通用</li>
+                </ul>
+                 <ul>
+                    <li>规格类型：正常规格</li>
+                    <li>是否量贩装：否</li>
+                    <li>原产国/地区：日本</li>
+                </ul>
+                <ul>
+                    <li>包装规格：6袋</li>
+                </ul>
+                </div>
+                <div style="margin-top: 50px;">
+                <img src="//img.alicdn.com/imgextra/i4/2200805177137/O1CN01AIKPf122anUmSVAR2_!!2200805177137-0-scmitem6000.jpg" align="absmiddle" width="1200">
+                <img src="//img.alicdn.com/imgextra/i1/2200805177137/O1CN010UECJ322anUogMetR_!!2200805177137-0-scmitem6000.jpg" align="absmiddle" width="1200">
+                <img src="//img.alicdn.com/imgextra/i1/2200805177137/O1CN01eHokI222anUrkenOI_!!2200805177137-0-scmitem6000.jpg" align="absmiddle" width="1200">
+                </div>
+            </el-card>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
 const route = useRoute()
-console.log(route.params.id)
+const productId = ref<any>('')
+const detailData = ref<any>({})
+onMounted(() => {
+    productId.value = route.params.id
+    getDetail()
+})
+const getDetail = async () => {
+    let params = {
+        productId: productId.value,
+    }
+    const { code, data, message } = await request<any>({
+        url: '/api/public/productDetail',
+        method: 'post',
+        data: params
+    })
+    detailData.value = data
+}
 </script>
+
+<style scoped>
+.header {
+    display: flex;
+}
+.left{
+    width: 40%;
+}
+.shopImg {
+    width: 400px;
+    height: 400px;
+}
+
+.right {
+    width: 85%;
+    margin-left: 20px;
+    height: 400px;
+}
+
+.title {
+    font-size: 30px;
+    font-weight: 900;
+}
+
+p {
+    margin-top: 20px;
+}
+.footer{
+    width: 100%;
+    margin-top: 20px;
+    display: flex;
+    flex-wrap: wrap;
+}
+.content{
+    width: 100%;
+}
+ul{
+    width: 100%;
+}
+li{
+    margin: 5px ;
+}
+</style>
--- pages/shopList.vue
+++ pages/shopList.vue
@@ -1,56 +1,85 @@
 <template>
   <div>
     <div class="header">
-      <el-avatar
-          :size="50"
-          src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-      />
+      <el-avatar :size="50" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" />
       <div>
         <h4 class="text-lg font-bold">店铺名称：杜蕾斯</h4>
         <p>店铺简介：<span>不成功便成人</span></p>
       </div>
     </div>
     <div class="w-full h-full grid grid-cols-4 gap-2 justify-center items-center mt-3">
-    <div v-for="i in 6" :key="i" class="rounded-lg border bg-card text-card-foreground shadow-sm" data-v0-t="card">
-    <div class="flex flex-col space-y-1.5 p-6">
-      <h2 class="text-xl font-bold">name</h2>
-      <div style="color: aliceblue;"
-        class="inline-flex items-center rounded-full border px-2.5 py-0.5 w-fit text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80">
-        有机产品
-      </div>
-    </div>
-    <div class="p-6">
-      <img src="../xx.jpg" alt="Apple" class="w-full h-64 object-cover" width="200" height="200"
-        style="aspect-ratio: 200 / 200; object-fit: cover;" />
-      <p class="mt-4">title</p>
-      <div class="flex items-center justify-between mt-4">
-        <div class="flex items-end justify-between">
-          <span class="text-lg font-bold">¥22222/</span>
-          <span class="text-sm font-bold">500g</span>
+      <div v-for="(item,index) in dataList" :key="index" class="rounded-lg border bg-card text-card-foreground shadow-sm" data-v0-t="card">
+        <div class="flex flex-col space-y-1.5 p-6">
+          <h2 class="text-xl font-bold">{{ item.name }}</h2>
+          <div style="color: aliceblue;"
+            class="inline-flex items-center rounded-full border px-2.5 py-0.5 w-fit text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80">
+            有机产品
+          </div>
+        </div>
+        <div class="p-6">
+          <img :src="item.cover" alt="Apple" class="w-full h-64 object-cover" width="200" height="200"
+            style="aspect-ratio: 200 / 200; object-fit: cover;" />
+          <p class="mt-4">{{ item.title }}</p>
+          <div class="flex items-center justify-between mt-4">
+            <div class="flex items-end justify-between">
+              <span class="text-lg font-bold">¥{{item.price}}/</span>
+              <span class="text-sm font-bold">500g</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  </div>
+    <div class="w-full justify-center flex mt-2 pb-5">
+        <el-pagination layout="prev, pager, next"   @size-change="handleSizeChange"
+      @current-change="handleCurrentChange" :current-page="page" :page-size="limit" :total="total" />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
 const route = useRoute()
 const storeId = ref<any>('')
-  onMounted(()=>{
-    storeId.value = route.query.storeId as any
-  })
+const page = ref<any>(1)
+const limit = ref<any>(10)
+const total = ref<any>(0)
+const dataList = ref<any>([])
+onMounted(() => {
+  storeId.value = route.query.storeId as any
+  getList()
+})
+const getList = async() =>{
+  let params = {
+    storeId: storeId.value,
+    page: page.value,
+    limit: limit.value
+  }
+  const { code, data, message } = await request<any>({
+        url: '/api/public/storeProducts',
+        method: 'post',
+        data: params
+    })
+    total.value = data.total
+    dataList.value = data.list
+}
+const handleSizeChange = (val: number) => {
+  limit.value = val
+}
+const handleCurrentChange = (val: number) => {
+  page.value = val
+  getList()
+}
 </script>
 <style>
-.header{
+.header {
   display: flex;
   align-items: center;
   margin-top: 20px;
-  div{
+
+  div {
     margin-left: 20px;
-    p{
-      span{
+
+    p {
+      span {
         font-size: 14px;
       }
     }
